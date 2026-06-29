e8fc8a02c5e16e738485d12bebd267a768931690
aliabbasiudev <aliabbasiudev@gmail.com>
2026-06-29 12:59:12 -0700

    fix gemini model name

--- api/gemini.js
+++ api/gemini.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
     const { prompt } = req.body
 
     const response = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent`,
+       `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent`,
         {
             method: 'POST',
             headers: {
